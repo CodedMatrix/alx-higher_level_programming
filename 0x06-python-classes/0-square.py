0d2e253c14e735dd9fd1f95fd0d389a0452f35c4
CodedMatrix <umuhozaallykhaled@gmail.com>
2024-10-01 15:31:58 -0700

    python classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class `Square`.
+"""
+
+# Define an empty class `Square`
+class Square:
+    """
+    Represents a square.
+    
+    This class is currently empty and only used to define a square.
+    """
+    # The pass statement is used because the class has no methods or attributes
+    pass
+
