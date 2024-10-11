2f8b35c80571b0b355f81ba98f24f06c29934232
CodedMatrix <umuhozaallykhaled@gmail.com>
2024-10-10 17:11:57 -0700

    test driven development

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+def add_integer(a, b=98):
+    """Adds two integers.
+
+    Args:
+        a (int or float): The first number.
+        b (int, optional): The second number. Defaults to 98.
+
+    Raises:
+        TypeError: If either a or b is not an integer or float.
+
+    Returns:
+        int: The sum of a and b, casted to an integer if necessary.
+    """
+
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise TypeError("a must be an integer or b must be an integer")
+
+    # Cast to integers if necessary
+    a = int(a) if isinstance(a, float) else a
+    b = int(b) if isinstance(b, float) else b
+
+    return a + b
